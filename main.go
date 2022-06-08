c2effec4f31baeb422a3867640a4294181dc6925
kurzepajedrzej <48054221+kurzepajedrzej@users.noreply.github.com>
2022-06-08 19:24:06 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 )
 
-var port string
-
 func main() {
-	static := http.FileServer(http.Dir("."))
-	if len(os.Args) > 1 {
-		port = os.Args[1]
-	} else {
-		port = "8080"
-	}
+	// reqeusts to the path /goodbye with be handled by this function
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
 
-	http.Handle("/", static)
-	fmt.Println("[*] - Server Listening on Port " + port)
-	fmt.Println("[*] - http://localhost:" + port + "/")
-	http.ListenAndServe(":"+port, nil)
+	// any other request will be handled by this function
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Running Hello Handler")
+
+		// read the body
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Error reading body", err)
+
+			http.Error(rw, "Unable to read request body", http.StatusBadRequest)
+			return
+		}
+
+		// write the response
+		fmt.Fprintf(rw, "Hello %s", b)
+	})
+
+	// Listen for connections on all ip addresses (0.0.0.0)
+	// port 9090
+	log.Println("Starting Server")
+	err := http.ListenAndServe(":9090", nil)
+	log.Fatal(err)
 }
